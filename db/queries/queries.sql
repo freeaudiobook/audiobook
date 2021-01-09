b4c9bbd19fc688a3bb6eb891b9e5a5982fd0657a
Prasanna Jagathesan <prasanna.jagathesan@avalara.com>
2021-01-09 17:14:59 +0530

    Add ordering for fetch book queries

--- db/queries/queries.sql
+++ db/queries/queries.sql
@@ -6,7 +6,7 @@ INSERT INTO PLAYSTATE(user_id, book_chapter, seek_position) values ($1, $2, $3)
 ON CONFLICT (user_id, book_chapter) DO UPDATE SET seek_position = $3;
 
 -- name: FetchBooksByTitleAndAuthor :many
-SELECT * FROM BOOKS where title LIKE '%' || $1 || '%' and author LIKE '%' || $2 || '%';
+SELECT * FROM BOOKS where title LIKE '%' || $1 || '%' and author LIKE '%' || $2 || '%' ORDER BY title;
 
 -- name: FetchBooksByGenre :many
-SELECT * FROM BOOKS where genre like '%' || $1 || '%';
+SELECT * FROM BOOKS where genre like '%' || $1 || '%' ORDER BY title;
