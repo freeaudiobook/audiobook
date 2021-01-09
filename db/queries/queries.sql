613f958b98380ca2883c0c3b19ea64d7687d6f5a
Prasanna Jagathesan <prasanna.jagathesan@avalara.com>
2021-01-09 17:13:58 +0530

    Add search queries for books

--- db/queries/queries.sql
+++ db/queries/queries.sql
@@ -4,3 +4,9 @@ INSERT into PLAYSTATE(user_id, book_chapter, seek_position) values($1,$2, $3);
 -- name: UpdateSeekPosition :exec
 INSERT INTO PLAYSTATE(user_id, book_chapter, seek_position) values ($1, $2, $3)
 ON CONFLICT (user_id, book_chapter) DO UPDATE SET seek_position = $3;
+
+-- name: FetchBooksByTitleAndAuthor :many
+SELECT * FROM BOOKS where title LIKE '%' || $1 || '%' and author LIKE '%' || $2 || '%';
+
+-- name: FetchBooksByGenre :many
+SELECT * FROM BOOKS where genre like '%' || $1 || '%';
