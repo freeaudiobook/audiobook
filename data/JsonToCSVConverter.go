7e5a5a302db2a64097ff661deec04abc2eb6a8bc
Mohanarangan Muthukumar <mohan@extrasalt.org>
2021-01-09 20:28:35 +0530

    Fix the csv generator

--- data/JsonToCSVConverter.go
+++ data/JsonToCSVConverter.go
@@ -11,24 +11,37 @@ import (
 )
 
 type Details struct {
-	title           string
-	author          string
-	genre           []string
-	description     string
-	language        string
-	librivoxPageUrl string
-	totalDuration   string
+	Title            string   `json:"title"`
+	AuthorID         int      `json:"authorId"`
+	Author           string   `json:"author"`
+	Genres           []string `json:"genres"`
+	YearOfPublishing int      `json:"yearOfPublishing"`
+	Language         string   `json:"language"`
+	ReaderID         int      `json:"readerId"`
+	Reader           string   `json:"reader"`
+	YoutubeVideoURL  string   `json:"youtubeVideoUrl"`
+	LibrivoxPageURL  string   `json:"librivoxPageUrl"`
+	TotalDuration    string   `json:"totalDuration"`
+	Description      string   `json:"description"`
+}
+
+type Track struct {
+	Title    string `json:"title"`
+	URL      string `json:"url"`
+	Duration string `json:"duration"`
 }
 
 type Row struct {
-	_id     map[string]string
-	details Details
-	tracks  []map[string]string
-	id      int
+	ID2 struct {
+		Oid string `json:"oid"`
+	} `json:"_id"`
+	Details Details `json:"details"`
+	Tracks  []Track `json:"tracks"`
+	ID      int     `json:"id"`
 }
 
 type Rows struct {
-	data []Row `json:"data"`
+	Data []Row `json:"data"`
 }
 
 func main() {
@@ -40,13 +53,11 @@ func main() {
 
 	var jsonData Rows
 
-	err = json.Unmarshal([]byte(jsonDataFromFile), &jsonData)
+	err = json.Unmarshal(jsonDataFromFile, &jsonData)
 
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
-
-	fmt.Println(len(jsonData.data))
 
 	file, err := os.Create("audiobooks.csv")
 	if err != nil {
@@ -57,16 +68,16 @@ func main() {
 
 	writer.Write([]string{"title", "librivox_url", "genre", "author", "summary", "language", "total_duration"})
 
-	for _, r := range jsonData.data {
+	for _, r := range jsonData.Data {
 		var record []string
 
-		record = append(record, r.details.title)
-		record = append(record, r.details.librivoxPageUrl)
-		record = append(record, strings.Join(r.details.genre, ","))
-		record = append(record, r.details.author)
-		record = append(record, r.details.description)
-		record = append(record, r.details.language)
-		record = append(record, r.details.totalDuration)
+		record = append(record, r.Details.Title)
+		record = append(record, r.Details.LibrivoxPageURL)
+		record = append(record, strings.Join(r.Details.Genres, ","))
+		record = append(record, r.Details.Author)
+		record = append(record, r.Details.Description)
+		record = append(record, r.Details.Language)
+		record = append(record, r.Details.TotalDuration)
 
 		writer.Write(record)
 	}
