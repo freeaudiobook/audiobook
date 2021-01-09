dc4ef64ce734158cd49f3e3911386e08641598c3
Prasanna Jagathesan <prasanna.jagathesan@avalara.com>
2021-01-09 16:48:31 +0530

    Add book table

--- db/migrations/20210109111343_add-book-info.sql
+++ db/migrations/20210109111343_add-book-info.sql
@@ -1,5 +1,13 @@
 -- migrate:up
-
+CREATE TABLE BOOKS (
+	book_id UUID,
+	title string,
+    image_url string,
+    librivox_url string,
+    genre string,
+    author string,
+    summary string
+)
 
 -- migrate:down
-
+DELETE TABLE BOOKS
