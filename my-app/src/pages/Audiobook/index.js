ea4db462aa87232f55906b2e7e5552dc5a482ffb
Harsh Agrawal <harsh.agarwal@avalara.com>
2021-01-10 21:03:38 +0530

    Add loading anim & genre page. Improve styling

--- my-app/src/pages/Audiobook/index.js
+++ my-app/src/pages/Audiobook/index.js
@@ -6,9 +6,9 @@ import { readBook } from '../../utils/api'
 
 import './style.css'
 
-function Audiobook({ match }){
+function Audiobook({ match }) {
     document.title = `${"Audiobook"} | The Book Hub`
-    
+
     const audiboookId = match.params.id
 
     const { setCurrentAudio } = useContext(GlobalContext)
@@ -29,55 +29,70 @@ function Audiobook({ match }){
     )
 
     useEffect(() => {
-        const func = async() => {
+        const func = async () => {
             setLoaded(false)
             const response = await readBook(audiboookId)
             setLoaded(true)
-            if(response.status !== 200){
+            if (response.status !== 200) {
                 return
             }
             setAudiobook({
                 title: response.data.Title,
                 chapters: response.data.Chapters
-            })            
+            })
         }
         func()
-        
+
     }, [audiboookId])
 
     return (
         <div className="audiobook-page rest-page">
-            <div className="top" >
-                <div className="bg-img" style={{ background: `url("${audiobook?.image_url?.String}")` }}/> 
-                <div className="cover-art">
-                    <img 
-                    src={audiobook?.image_url?.String}
-                    />
-                </div>
-                <div className="name-and-author">
-                    <h1 className="title">
-                        {audiobook?.title?.String}
-                    </h1>
-                    <h3 className="author">
-                        By {audiobook?.author?.String}
-                    </h3>
-                </div>
-            </div>
-            <br/>
-            <h3>Chapters</h3>
-            <div className="chapters">
-                {
-                    (audiobook?.chapters || []).map((chapter, _) => 
-                        <div 
-                            className="chapter"
-                            onClick={() => updateCurrentAudio(chapter)}
-                        >
-                            <img src={audiobook.Link}/>
-                            <h4>{chapter.Title}</h4>
+            {
+                loaded
+                &&
+                <>
+                    <div className="top" >
+                        <div className="bg-img" style={{ background: `url("${audiobook?.image_url?.String}")` }} />
+                        <div className="cover-art">
+                            <img
+                                src={audiobook?.image_url?.String}
+                            />
                         </div>
-                    )
-                }
-            </div>
+                        <div className="name-and-author">
+                            <h1 className="title">
+                                {audiobook?.title?.String}
+                            </h1>
+                            <h3 className="author">
+                                By {audiobook?.author?.String}
+                            </h3>
+                        </div>
+                    </div>
+                    <br />
+                    <h3>Chapters</h3>
+                    <div className="chapters">
+                        {
+                            (audiobook?.chapters || []).map((chapter, _) =>
+                                <div
+                                    className="chapter"
+                                    onClick={() => updateCurrentAudio(chapter)}
+                                >
+                                    <img src={audiobook.Link} />
+                                    <h4>{chapter.Title}</h4>
+                                </div>
+                            )
+                        }
+                    </div>
+                </>
+            }
+            {
+                !loaded
+                &&
+                <div class="loading">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            }
         </div>
     )
 }
